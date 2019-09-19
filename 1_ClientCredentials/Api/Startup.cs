7e5d1f8cb2fe9c0333a1d8495314067ba8ce325d
Zhengchen Tao <v-zhetao@microsoft.com>
2019-09-19 17:28:57 +0800

    complete demo 1

--- 1_ClientCredentials/Api/Startup.cs
+++ 1_ClientCredentials/Api/Startup.cs
@@ -23,7 +23,7 @@ namespace Api
             services.AddAuthentication("Bearer")
             .AddJwtBearer("Bearer", options =>
             {
-                options.Authority = "https://localhost:5000";
+                options.Authority = "http://localhost:5000";
                 options.RequireHttpsMetadata = false;
 
                 options.Audience = "api1";
